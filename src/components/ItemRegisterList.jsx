356514fe3a1862b696451aad57c9946e9747b291
Leandro <leandrovillafu@gmail.com>
2023-06-16 17:44:27 -0300

    Registers screen shows list

--- src/components/ItemRegisterList.jsx
+++ src/components/ItemRegisterList.jsx
@@ -1,13 +1,49 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableWithoutFeedback, StyleSheet } from "react-native";
 import React from "react";
 
 const ItemRegisterList = ({ item }) => {
-  console.log(item);
   return (
-    <View>
-      <Text>ItemRegisterList</Text>
-    </View>
+    <TouchableWithoutFeedback
+      style={styles.container}
+      onPress={() => console.log(`%c${"hola" + item.id}`, "color:red")}
+    >
+      <View style={styles.card}>
+        <View style={styles.id}>
+          <Text>#{item.id}</Text>
+        </View>
+        <View style={styles.date}>
+          <Text>{item.time} hs</Text>
+          <Text>{item.date}</Text>
+        </View>
+        <View style={styles.name}>
+          <Text style={styles.nameText}>{item.name}</Text>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginHorizontal: "auto",
+    top: 20,
+  },
+  card: {
+    alignItems: "center",
+    flexDirection: "row",
+    paddingLeft: 20,
+    paddingRight: 20,
+    marginBottom: 10,
+    marginHorizontal: 10,
+    paddingVertical: 5,
+    backgroundColor: "#3f70d499",
+    borderRadius: 10,
+  },
+  id: { flex: 1 },
+  date: { flex: 2 },
+  name: { flex: 3 },
+  nameText: { fontSize: 20 },
+});
 
 export default ItemRegisterList;
--- src/components/RegisterList.jsx
+++ src/components/RegisterList.jsx
@@ -1,30 +1,69 @@
-import { FlatList, ActivityIndicator } from "react-native";
+import {
+  FlatList,
+  Text,
+  ActivityIndicator,
+  StyleSheet,
+  View,
+} from "react-native";
 import React from "react";
 import registers from "../utils/mockups/registers.js";
 import ItemRegisterList from "./ItemRegisterList.jsx";
 
 const RegisterList = () => {
   return (
-    <FlatList
-      data={registers}
-      // numColumns={2}
-      showsVerticalScrollIndicator={false}
-      keyExtractor={(item) => item.id}
-      renderItem={({ item }) => <ItemRegisterList item={item} />}
-      // contentContainerStyle={styles.flatListContentContainer}
-      //   onEndReached={isNext && laodMore}
-      //   onEndReachedThreshold={0.1}
-      // ListFooterComponent={
-      //   //   isNext && (
-      //   <ActivityIndicator
-      //     size="large"
-      //     // style={styles.spinner}
-      //     color="#AEAEAE"
-      //   />
-      //   //   )
-      // }
-    />
+    <>
+      <View style={styles.listHeader}>
+        <Text style={styles.textHeaderId}>No</Text>
+        <Text style={styles.textHeaderDate}>Fecha</Text>
+        <Text style={styles.textHeaderName}>Nombre</Text>
+      </View>
+      <FlatList
+        data={registers}
+        // numColumns={2}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <ItemRegisterList item={item} />}
+        contentContainerStyle={styles.flatListContentContainer}
+        //   onEndReached={isNext && laodMore}
+        //   onEndReachedThreshold={0.1}
+        // ListFooterComponent={
+        //   //   isNext && (
+        //   <ActivityIndicator
+        //     size="large"
+        //     // style={styles.spinner}
+        //     color="#AEAEAE"
+        //   />
+        //   //   )
+        // }
+      />
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  flatListContentContainer: {
+    bottom: 30,
+    top: 20,
+  },
+  listHeader: {
+    flexDirection: "row",
+    backgroundColor: "#AEAEAE",
+    paddingHorizontal: 30,
+    height: 30,
+    alignItems: "center",
+  },
+  textHeaderId: {
+    flex: 1,
+    fontWeight: "bold",
+  },
+  textHeaderDate: {
+    flex: 2,
+    fontWeight: "bold",
+  },
+  textHeaderName: {
+    flex: 3,
+    fontWeight: "bold",
+  },
+});
 
 export default RegisterList;

0ac3e45ac407e6a8a97699775a45abe3504a2c49
Leandro <leandrovillafu@gmail.com>
2023-07-03 16:26:09 -0300

    Refactor form object, mock up record

--- src/screens/RecordDetails.jsx
+++ src/screens/RecordDetails.jsx
@@ -1,16 +1,60 @@
-import { View, Text } from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
 import React, { useState, useCallback } from "react";
+import { Avatar, Button, Card, Text } from "react-native-paper";
 import useWithoutHeader from "../hooks/useWithoutHeader";
+import Icon from "react-native-vector-icons/FontAwesome5";
 
 const RecordDetails = (props) => {
-  const { navigation, route } = props;
+  const {
+    navigation,
+    route: {
+      params: { id },
+    },
+  } = props;
 
   useWithoutHeader();
   return (
-    <View>
-      <Text>RecordDetails</Text>
-    </View>
+    <ScrollView>
+      <Card>
+        <Card.Content style={{ padding: 5 }}>
+          <Text variant="headlineSmall">Registro #{id}</Text>
+        </Card.Content>
+        <Card.Content style={styles.head}>
+          <View style={styles.name}>
+            <Text variant="titleLarge">Usuario: Bruce Wayne</Text>
+          </View>
+          <View style={styles.time}>
+            <Icon name="clock">07:40hs, 11 May, 2023</Icon>
+          </View>
+        </Card.Content>
+        <Card.Content style={styles.list}>
+          <ScrollView style={styles.items}>
+            <View style={styles.listItemTitle}>
+              <Icon name="pen" size={15} />
+              <View style={{ paddingLeft: 10 }}>
+                <Text variant="bodyLarge">EQUISDE</Text>
+              </View>
+            </View>
+            <View>
+              <View>
+                <Text variant="bodyMedium">EQUISDE</Text>
+              </View>
+            </View>
+          </ScrollView>
+        </Card.Content>
+      </Card>
+    </ScrollView>
   );
 };
 
+const styles = StyleSheet.create({
+  time: { paddingVertical: 5 },
+  items: { paddingVertical: 20 },
+  head: { padding: 5, display: "flex", alignItems: "flex-end" },
+  listItemTitle: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+});
 export default RecordDetails;
---
+++ src/utils/mockups/record.js
@@ -0,0 +1,43 @@
+export default {
+  form: [
+    {
+      checked: "Coincide",
+      comment: "",
+      id: 1,
+      title: "Sillas (84)",
+    },
+    {
+      checked: "No Coincide",
+      comment: "",
+      id: 2,
+      title: "Multimetros (18)",
+    },
+    {
+      checked: "Coincide",
+      comment: "",
+      id: 3,
+      title: "Puertas Cerradas (4)",
+    },
+    {
+      checked: "No Coincide",
+      comment: "",
+      id: 4,
+      title: "Calibres (X)",
+    },
+    {
+      checked: "Coincide",
+      comment: "",
+      id: 5,
+      title: "Tornillos (X)",
+    },
+    {
+      checked: "Coincide",
+      comment: "",
+      id: 6,
+      title: "Fuentes de laboratorio (5)",
+    },
+  ],
+  user: "laboratorio@gmail.com",
+  name: "Bruce Wayne",
+  id: 154,
+};
